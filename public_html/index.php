1fbbe968ac566f6c1e6917b5d0528ee0dce1ca4f
neight0903 <neight.td@gmail.com>
2016-12-15 19:45:23 +0900

    make chat comment log

--- public_html/index.php
+++ public_html/index.php
@@ -33,7 +33,7 @@ $app->run();
       <input type="hidden" id="token" value="<?= h($_SESSION['token']); ?>">
     </form>
     
-    <div id="chats"></div>
+    <ul id="chats"></ul>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
