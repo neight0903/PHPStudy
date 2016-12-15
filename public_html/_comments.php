e0cd7ba7cd493e05cb66eacfa941cfd067cea443
neight0903 <neight.td@gmail.com>
2016-12-15 19:56:07 +0900

    make chat comment log

--- public_html/_comments.php
+++ public_html/_comments.php
@@ -6,7 +6,7 @@ require_once(__DIR__ . '/../config/config.php');
 $app = new MyApp\Controller\Index();
 
 $chats = $app->getComment();
-
+echo $chats;
 foreach ($chats as $chat) {
   $name = h($chat->name);
   $comment = h($chat->comment);
