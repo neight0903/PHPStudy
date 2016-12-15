c0d4fa59e6b96042c3f66f41cd408cd2355d9fbd
neight0903 <neight.td@gmail.com>
2016-12-15 20:34:17 +0900

    make chat comment log

--- public_html/_ajax.php
+++ public_html/_ajax.php
@@ -6,9 +6,7 @@ $chatApp = new \MyApp\Contoller\Index();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   try {
-    $res = $todoApp->post();
-    header('Content-Type: application/json');
-    echo json_encode($res);
+    $todoApp->post();
     exit;
   } catch (Exception $e) {
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
