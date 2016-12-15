cd283d7b7a2ee75170fd438687d1e18bb29772c4
neight0903 <neight.td@gmail.com>
2016-12-15 15:37:54 +0900

    debugging

--- lib/Model/Chat.php
+++ lib/Model/Chat.php
@@ -3,7 +3,7 @@ namespace MyApp\Model;
 
 class Chat extends \MyApp\Model {
   public function getChats() {
-    $stmt = $this->_db->query("select * from chats order by id desc");
+    $stmt = $this->db->query("select * from chats order by id desc");
     $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
     return $stmt->fetchAll();
   }
