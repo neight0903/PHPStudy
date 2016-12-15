0df67cff65c0d02f945fa585689fdb019c0be30f
neight0903 <neight.td@gmail.com>
2016-12-15 19:04:59 +0900

    comp login

--- lib/Model/Chat.php
+++ lib/Model/Chat.php
@@ -6,14 +6,17 @@ class Chat extends \MyApp\Model {
   public function create($values) {
     $stmt = $this->db->prepare("insert into chats (user_id, comment) values (:user_id, :comment)");
 
-    $res = $stmt->execute([])
-    
+    $res = $stmt->execute([
+      ':user_id' => $values['user_id'],
+      ':comment' => $values['comment']
+    ]);
   }
 
   public function findAll() {
-    $stmt = $this->db->query("select * from chats order by id desc");
+    $stmt = $this->db->query("select chats.id, chats.comment, users.name from chats, users where chats.user_id = users.id order by chats.id desc;");
     $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
     return $stmt->fetchAll();
   }
 
 }
+
